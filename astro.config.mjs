a3843e5d79e65735c4bf223a56a7888fb9543869
Alexis Polo <apolo@MacBook-Pro-de-Alexis.local>
2025-05-04 21:41:13 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,31 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-    site: " https://awscommunitydayecuador.github.io/web/",
-    output: 'static',
-    base: '/',
-    integrations: [
-        compress({
-          css: true,
-          html: true,
-          js: true,
-          img: false,
-          svg: true,
-        }),
-      ],
-      build: {
-        assets: "_assets",
-      },
-      compressHTML: true,
+  site: " https://awscommunitydayecuador.github.io",
+  output: 'static',
+  base: '/',
+
+  integrations: [
+      compress({
+        css: true,
+        html: true,
+        js: true,
+        img: false,
+        svg: true,
+      }),
+    ],
+
+  build: {
+    assets: "_assets",
+  },
+
+  compressHTML: true,
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
